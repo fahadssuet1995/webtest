0dccd55ec2436eefddf93ec69d817ccbe168fe2d
fahadssuet1995 <57964362+fahadssuet1995@users.noreply.github.com>
2024-03-11 12:13:54 +0500

    htmls and open files

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyBeUNbYNaqXbWHxH4CyFMAUyqFYDQd38Ic",
+    authDomain: "betteragent-776b4.firebaseapp.com",
+    databaseURL: "https://betteragent-776b4-default-rtdb.firebaseio.com",
+    projectId: "betteragent-776b4",
+    storageBucket: "betteragent-776b4.appspot.com",
+    messagingSenderId: "638601102700",
+    appId: "1:638601102700:web:2d7036e87f460179f7df49"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const database = getDatabase(app);
+
+  export { database };
+
+  console.log("hello moto");
